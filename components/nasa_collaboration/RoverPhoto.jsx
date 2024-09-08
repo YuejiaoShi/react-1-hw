1026fa0846b4412268d1a5a8df7fb453fb90f02a
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-09 01:07:53 +0200

    fill RoverPhoto with date, rover, image displays

--- components/nasa_collaboration/RoverPhoto.jsx
+++ components/nasa_collaboration/RoverPhoto.jsx
@@ -1,9 +1,16 @@
-function RoverPhoto() {
-    return (
-        <div>
-            
-        </div>
-    )
+import styles from "./nasa_collaboration.module.css";
+function RoverPhoto({ src, date, roverName }) {
+  return (
+    <div className={styles.roverPhotoItem}>
+      <p>Date: {date}</p>
+      <p>Rover: {roverName}</p>
+      <img
+        className={styles.nasaPicOfTheDayImg}
+        src={src}
+        alt={`Photo taken by Rover ${roverName} on ${date}`}
+      />
+    </div>
+  );
 }
 
-export default RoverPhoto
+export default RoverPhoto;
